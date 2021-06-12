8c8a1ff2c3211e98f493c681439607f93630bcc7
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2021-06-12 03:13:21 +0300

    point 2 done

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -23,7 +23,7 @@ import { Button, Card, Container, Row, Col } from "reactstrap";
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
-import profile from "src/assets/img/theme/Yousef-AbuJalboush2.jpeg";
+import profile from "assets/img/theme/Yousef-AbuJalboush2.png";
 
 class Profile extends React.Component {
   componentDidMount() {
